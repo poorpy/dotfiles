62a6924fb7fc7d08707653bb84109402251c051c
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2020-02-25 21:32:52 +0100

    wip

--- nvim/coc.sh
+++
@@ -1,8 +0,0 @@
-base="coc-json coc-tsserver coc-tabnine coc-yank coc-marketplace coc-vimlsp"  
-home="coc-rls coc-vimtex"
-work="coc-python coc-go"
-
-extensions="${base} ${home} ${work}"
-
-vim -c "CocInstall -sync ${extensions}|q"
-
--- nvim/install.sh
+++ nvim/install.sh
@@ -7,3 +7,14 @@ ln -s "${SCRIPTPATH}" ~/.config/nvim
 # Download vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install plugins
+vim -c "PlugInstall|q"
+
+base="coc-json coc-tsserver coc-tabnine coc-yank coc-marketplace coc-vimlsp"  
+home="coc-rls coc-vimtex"
+work="coc-python coc-go"
+
+extensions="${base} ${home} ${work}"
+
+vim -c "CocInstall -sync ${extensions}|q"
