b6b4bc8e5dbbae890ed2a6f4585f36d24e4edbff
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2022-03-19 23:25:05 +0100

    minor fixes
    * use consistent quoting convention
    * remove unused exports

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,19 +1,17 @@
 #!/bin/env zsh
 
-alias grep='grep --color=auto'
-alias ls='ls --color=auto'
-alias ll='ls -l'
-alias :q='exit'
-alias cls='clear; ls'
-alias cll='clear; ll'
-alias gdb='gdb -quiet'
+alias grep="grep --color=auto"
+alias ls="ls --color=auto"
+alias ll="ls -l"
+alias :q="exit"
 
 
-alias vimrc='cd ${HOME}/.files/nvim/; nvim init.vim; cd -; '
-alias zshrc='cd ${HOME}/.files/zsh/; nvim zshrc; cd -; '
-alias ocm-env="source ~/venv-ocm/bin/activate"
-alias clipboard='xclip -selection clipboard'
-alias primary='xclip -selection primary'
+alias vimrc="cd ${HOME}/.files/nvim/; nvim init.vim; cd -; "
+alias zshrc="cd ${HOME}/.files/zsh/; nvim zshrc; cd -; "
 
-alias tokiofmt="rustfmt --check --edition 2018 $(find . -name '*.rs' -print)"
-alias tokiodoc='RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --all-features'
+alias clipboard="xclip -selection clipboard"
+alias primary="xclip -selection primary"
+
+alias vim="noglob vim"
+alias ssh="noglob ssh"
+alias gdb="gdb -quiet"
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,25 +3,20 @@
 export EDITOR='nvr --remote -s'
 export READER='zathura'
 
-export PATH=$PATH:/home/poorpy/.bin:/home/poorpy/.cargo/bin
-export PATH=$PATH:/home/poorpy/.local/bin
-export PATH=$PATH:/home/poorpy/.emacs.d/bin
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-export PATH="$HOME:/.local/bin:$PATH"
-export PATH="$PATH:/usr/lib/go/bin:$HOME/go/bin"
-export PATH="$PATH:$HOME/.nimble/bin"
-export PATH="$PATH:$HOME/.luarocks/bin"
 
 
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
-export PYTHONPATH="$PYTHONPATH:$HOME/git/amg/src"
+export PYTHONPATH="$PYTHONPATH:${HOME}/git/amg/src"
 export GOPATH=$HOME/.go
-# export GOROOT=$HOME/.goroot
 export GOBIN=$HOME/.gobin
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:$GOBIN
+
+export PATH="$PATH:${HOME}/.cargo/bin:${HOME}/.local/bin"
+export PATH="$PATH:${HOME}/.local/bin"
+export PATH="$PATH:${HOME}/.yarn/bin:${HOME}/.config/yarn/global/node_modules/.bin"
+export PATH="$PATH:${HOME}/.luarocks/bin"
+export PATH="$PATH:$GOPATH/bin:$GOBIN"
 
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
-
