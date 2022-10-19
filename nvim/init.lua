f3ad2c51fcbb3221b946ce5a5061cc6eb1debe4b
Bartosz Marczynski <marczynski.bartosz@gmail.com>
2022-10-19 12:53:03 +0200

    feat: add nvim-dap, add spellcheck commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,5 @@ require("autocmds")
 
 vim.cmd([[ colorscheme deep-space ]])
 vim.cmd([[ hi MatchParen guifg=#c47ebd guibg=#51617d ]])
+vim.cmd([[ command! Pol execute ":set spelllang=pl spell"]])
+vim.cmd([[ command! Eng execute ":set spelllang=en spell"]])
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -207,4 +207,30 @@ return require("packer").startup(function(use)
         end,
     })
     -- }}}
+
+    -- nvim-dap {{{
+    use({
+        "mfussenegger/nvim-dap",
+        opt = true,
+        event = "BufReadPre",
+        module = { "dap" },
+        wants = {
+            "nvim-dap-virtual-text",
+            "nvim-dap-ui",
+            "nvim-dap-python",
+            "which-key.nvim",
+        },
+        requires = {
+            "theHamsta/nvim-dap-virtual-text",
+            "rcarriga/nvim-dap-ui",
+            "mfussenegger/nvim-dap-python",
+            "nvim-telescope/telescope-dap.nvim",
+            { "leoluz/nvim-dap-go", module = "dap-go" },
+            { "jbyuki/one-small-step-for-vimkind", module = "osv" },
+        },
+        config = function()
+            require("config.dap").setup()
+        end,
+    })
+    -- }}}
 end)
